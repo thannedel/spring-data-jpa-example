2ed5e6c1590d61fe77926a81469536e1778835e7
thannedel <57347092+thannedel@users.noreply.github.com>
2020-07-12 12:55:27 +0200

    init

---
+++ src/test/java/spring/data/jpa/api/SpringDataJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data.jpa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
